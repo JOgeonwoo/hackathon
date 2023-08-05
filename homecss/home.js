4b5364edba213a62d3b3af2aabd221332b331570
copybaraa <140491692+copybaraa@users.noreply.github.com>
2023-08-06 00:00:26 +0900

    Add files via upload

---
+++ homecss/home.js
@@ -0,0 +1,21 @@
+function zoomIn(event) {
+    document.getElementById("gift-icon").src = "https://cdn-icons-png.flaticon.com/128/2420/2420548.png"
+    event.target.style.transform = "scale(1.2)";
+    event.target.style.zIndex = 1;
+    event.target.style.transition = "all 0.5s";
+}
+function zoomOut(event) {
+    document.getElementById("gift-icon").src = "https://cdn-icons-png.flaticon.com/128/2420/2420548.png"
+    event.target.style.transform = "scale(1)";
+    event.target.style.zIndex = 0;
+    event.target.style.transition = "all 0.5s";
+}
+function nextPage1() {
+    location.href = "Step1.html";
+}
+function nextPage2() {
+    window.location.href = 'http://localhost:3000/login';
+}
+function nextPage3() {
+    location.href = "http://localhost:3000/signup";
+}
